85609780b4a28a763b7ba63e2a7e72331e4569c9
Darkhan Zhursin <darkhan.zhursin@mediamonks.com>
2023-01-06 16:50:47 +0600

    Lesson 6 | changed query (not hard coded)

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-Select * from PAYMENT where type_id=4;
-select * from MARK where SUBJECT_ID=92;
-select * from STUDENT where id IN (select STUDENT_ID from PAYMENT where TYPE_ID = 4);
-select * from STUDENT where id IN (select STUDENT_ID from MARK where SUBJECT_ID = 104);
+select * from PAYMENT where type_id=(select id from PAYMENTTYPE where NAME like 'MONTHLY');
+select * from MARK where SUBJECT_ID=(select id from SUBJECT where NAME like 'Art');
+select * from STUDENT where id IN (select STUDENT_ID from PAYMENT where TYPE_ID = ((select id from PAYMENTTYPE where NAME like 'WEEKLY')));
+select * from STUDENT where id IN (select STUDENT_ID from MARK where SUBJECT_ID = (select id from SUBJECT where NAME like 'Math'));
